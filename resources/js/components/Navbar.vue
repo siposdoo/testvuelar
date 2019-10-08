686f97e0ba25c2787f958380006957e905d99da5
Stanislav Despotovic <ssttaanniissllaavv@gmail.com>
2019-10-08 12:58:40 +0200

    Komponenta za navbar sa opcijama sakrivanja i pokazivanja u odnosu da li je user logovan ili ne

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,14 +1,35 @@
 <template>
-<nav class="navbar navbar-expand-sm navbar-dark bg-info mb-2">
-    <div class="contaner">
-    <a href="/" class="navbar-brand">Pocetna</a>
+ <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+               <a href="/" class="navbar-brand">Pocetna</a>
   
-    <a  v-if="logovan" href="/administracija" class="navbar-brand">Administracija</a>
-    <a  v-if="logovan" href="/logout" class="navbar-brand">Odjavi se</a>
-      <a  v-if="!logovan" href="/vuelogin" class="navbar-brand">Prijava</a>
-
+            </li>
+            <li class="nav-item">
+               <a  v-if="logovan" href="/administracija" class="navbar-brand">Citati</a>
+            </li>
+              <li class="nav-item">
+               <a  v-if="logovan" href="/subscribers" class="navbar-brand">Pretplatnici</a>
+            </li>
+            
+        </ul>
+    </div>
+    <div class="mx-auto order-0">
+       
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a  v-if="logovan" href="/logout" class="navbar-brand">Odjavi se</a>
+            </li>
+            <li class="nav-item">
+                <a  v-if="!logovan" href="/vuelogin" class="navbar-brand">Prijava</a>
+            </li>
+        </ul>
     </div>
 </nav>
+ 
 </template>
 <script>
     export default {
