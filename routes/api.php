36223ff54e559cac3dff31c8034e486c9c6b2626
Stanislav Despotovic <ssttaanniissllaavv@gmail.com>
2019-10-08 12:46:06 +0200

    Kreirao nove Route za subscribere

--- routes/api.php
+++ routes/api.php
@@ -20,16 +20,36 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // Cvi citati
 Route::get('articles','ArticleController@index');
 
+// Cvi pretplatnici
+Route::get('subscribers','SubscriberController@index');
+
 // Jedan citat
 Route::get('article/{id}','ArticleController@show');
 
+// Jedan pretplatnik
+Route::get('subscriber/{id}','SubscriberController@show');
+
+// Jedan pretplatnik po emailu
+Route::get('subscriberbyemail/{email}','SubscriberController@showbyemail');
+
 // Novi citat
 Route::post('article','ArticleController@store');
+
+
+// Novi pretplatnik
+Route::post('subscriber','SubscriberController@store');
+
+// Update pretplatnik
+Route::put('subscriber','SubscriberController@store');
 
 // Update citat
 Route::put('article','ArticleController@store');
 
 // Brisi citat
 Route::delete('article/{id}','ArticleController@destroy');
+
+
+// Brisi pretplatnika
+Route::delete('subscriber/{id}','SubscriberController@destroy');
 
 
