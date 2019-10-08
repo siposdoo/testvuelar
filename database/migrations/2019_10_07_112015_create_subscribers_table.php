db6c6741155937dde899fdbde78a36a759435a4d
Stanislav Despotovic <ssttaanniissllaavv@gmail.com>
2019-10-08 13:10:14 +0200

    Migration pretplatnika sa definisanim poljima :)

---
+++ database/migrations/2019_10_07_112015_create_subscribers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscribersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email')->unique();
+            $table->integer('payment')->default(0); 
+            $table->integer('sent_citat')->default(0);
+            $table->string('email_token')->unique();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscribers', function (Blueprint $table) {
+            $table->dropSoftDeletes(); //add this line
+        });
+    }
+}
