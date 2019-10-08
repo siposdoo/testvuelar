afa274540bb2112a025e5045e9cc119a86854be9
Stanislav Despotovic <ssttaanniissllaavv@gmail.com>
2019-10-08 12:57:51 +0200

    Component za pretplatnike sa metodama : fetchPretplatnici() :  Ova metoda kupi pretplatnike i dodjeljuje ih  objektu pretplatnici
    makePagination():
      Uzimamo paginaciju iz fetch i dobijamo linkove za napred ,nazad
    broj itema,....

---
+++ resources/js/components/Pretplatnici.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <h2>Pretplatnici {{totalc}}</h2>
+    
+ 
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-bind:class="[{disabled:!pagination.prev_page_url}]" class="page-item">
+          <a @click="fetchPretplanici(pagination.prev_page_url);" class="page-link" href="#">Prethodna</a>
+        </li>
+        <li class="page-item disabled">
+          <a class="page-link">{{pagination.current_page}} od {{pagination.last_page}}</a>
+        </li>
+        <li v-bind:class="[{disabled:!pagination.next_page_url}]" class="page-item">
+          <a @click="fetchPretplanici(pagination.next_page_url);" class="page-link" href="#">Sledeca</a>
+        </li>
+        <li>
+          <select name="tippretplate" @change="filterPretplata($event)"    class="form-control">
+     <option value="-1">Svi</option>
+     <option value="0">Nema pretplate</option>
+     <option value="1">Ima pretplatu</option>
+</select>
+          </li>
+      </ul>
+    </nav>
+    
+     <table  class="table" >
+       <thead class="thead-dark">
+          <tr>
+       <th>Email</th>
+    
+       <th>Datum registovanja</th>
+          <th>Pretplata</th>
+        </tr>
+        </thead>
+         <tbody>
+    <tr  v-for="pretplanik in pretplanici" v-bind:key="pretplanik.id">
+      <td>{{pretplanik.email}}</td>
+      <td>{{pretplanik.created_at}}</td>
+      <td><span v-if="pretplanik.payment==1" class="badge badge-success">Pretplata</span><span v-else class="badge badge-warning">Nema pretplate</span></td>
+      
+     
+    </tr>
+     </tbody>
+     </table>
+     
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      pretplanici: [],
+      totalc: 0,
+      pretplanik: {
+        id: "",
+        email: "",
+        payment: "",
+        payment: "",
+
+
+      },
+      sub_id: "",
+      pagination: {},
+      edit: false
+    };
+  },
+  created() {
+    this.fetchPretplanici();
+  },
+  methods: {
+    filterPretplata(event){
+ this.fetchPretplanici(this.pagination.current_page_url+'&filter='+event.target.value);
+    },
+    fetchPretplanici(url_p) {
+      let mm = this;
+      url_p = url_p || "api/subscribers";
+      fetch(url_p)
+        .then(res => res.json())
+        .then(res => {
+          this.pretplanici = res.data;
+          mm.makePagination(res.meta, res.links);
+        })
+        .catch(err => console.log(err));
+    },
+    makePagination(m, l) {
+      let pagination = {
+        current_page: m.current_page,
+        last_page: m.last_page,
+        next_page_url: l.next,
+        current_page_url: "api/subscribers?page=" + m.current_page,
+        prev_page_url: l.prev
+      };
+      this.totalc = m.total;
+      this.pagination = pagination;
+    }
+   
+  }
+};
+</script>>
